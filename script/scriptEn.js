d80165f0c8c4172d36d7771398798b975d99ebfb
Gustavo Rotta <gustavo.rotta@hotmail.com>
2022-07-07 17:05:23 -0300

    Update

--- script/scriptEn.js
+++ script/scriptEn.js
@@ -38,7 +38,7 @@ const personalLinks = [
     {
         name: "https://github.com/GusRot",
         href: "https://github.com/GusRot",
-        link: "https://bn02pap001files.storage.live.com/y4mfz4ITgsi8lZ7AASAiUrvXV9eICS12npEla7O_fCb5FHO_PBxRwd6ViK-VfbyFKkuBJCb0Qkop84TK_ddzqUrsSr5MdtnrPVhmGgaJHnldJuXaAsAVpOnZMVwgnFNm9FiHKGlf6naxr7EWT18fQ_WqVY-R6MYsW67CWtNLgoDCdl44-qP4CR068n_NjrCvdYE?width=450&height=428&cropmode=none",
+        src: "https://bn02pap001files.storage.live.com/y4mfz4ITgsi8lZ7AASAiUrvXV9eICS12npEla7O_fCb5FHO_PBxRwd6ViK-VfbyFKkuBJCb0Qkop84TK_ddzqUrsSr5MdtnrPVhmGgaJHnldJuXaAsAVpOnZMVwgnFNm9FiHKGlf6naxr7EWT18fQ_WqVY-R6MYsW67CWtNLgoDCdl44-qP4CR068n_NjrCvdYE?width=450&height=428&cropmode=none",
     },
 ];
 
@@ -79,23 +79,12 @@ const languages = [
     },
 ];
 
-const professional = [
-    {
-        title: "Project Manager Softmakers",
-        period: " May / 2021 – October / 2021",
-        ativities: {
-            ativitie:
-                "Management of incremental deliveries of development team through the Scrum framework, divided into Sprints driven by a goal in each cycle.",
-            objectives:
-                "Bring digital transformation to companies according to the needs of each business.",
-            improve: "People and resources management and conflict management.",
-        },
-    },
+const professionalComplementary = [
     {
         title: "Financial Consultant freelance",
-        period: "Agosto / 2020 – Junho / 2021",
-        ativities: {
-            ativitie:
+        period: "August / 2020 – June / 2021",
+        activities: {
+            activity:
                 "Responsible for analyzing and evaluating the financial life of customers using the best financial product options that the Brazilian market offers according to each profile.",
             objectives:
                 "Develop the best strategy for customers to achieve their goals.",
@@ -106,8 +95,8 @@ const professional = [
     {
         title: "Commercial Consultant STONE ",
         period: "January / 2020 – August / 2020",
-        ativities: {
-            ativitie:
+        activities: {
+            activity:
                 "Prospecting and attracting customers; portfolio analyses; Management of the sales funnel and churn prevention.",
             objectives:
                 "Increase clients base, help customers improve their business by increasing their sales, and increasing their NPS.",
@@ -118,8 +107,8 @@ const professional = [
     {
         title: "PCP Intern VITAMASSA(Industry)",
         period: "July / 2019 – December / 2019",
-        ativities: {
-            ativitie:
+        activities: {
+            activity:
                 "Responsible for production planning and control activities; Use of ERP Protheus in resupply process; Framework Kaizen.",
             objectives:
                 "Ensure resources and supplies at the right time at the right place, in addition to an improvement project with the application of Six Sigma methodology in the Supply process. ",
@@ -129,9 +118,9 @@ const professional = [
     },
     {
         title: "Manager Ciclo Consultoria - Junior Company",
-        period: "December / 2017 – December / 2018",
-        ativities: {
-            ativitie:
+        period: "Dec / 2017 – Dec / 2018",
+        activities: {
+            activity:
                 "Negotiation and Sale of Services; Planning and Preparation of Commercial Proposals; Company Event Representative; Project Planning and Control.",
             objectives:
                 "Enhance practical knowledge of the student community, in addition to the theoretical knowledge of the university. ",
@@ -141,13 +130,99 @@ const professional = [
     },
 ];
 
+const professional = [
+    {
+        title: "Adobe Commerce Developer - Webjump",
+        period: "March / 2022 – current",
+        ativities: {
+            ativitie:
+                "Company specialized in Adobe Commerce, where I work with Magento. I use the MVC framework in a docker environment with versioning on Bitbucket. Currently working with B2B",
+            objectives: "Develop fast and flexible storefronts",
+            improve:
+                "JS, KO, JQuery, LESS, XML, PHP, AJAX, Git, JIRA, LINUX, grunt",
+        },
+    },
+    {
+        title: "Developer Web React",
+        period: "February / 2021 – current",
+        activities: {
+            activity:
+                "Development of front-end applications in personal projects, maintenance, and implementation of new features. More than 20 completed projects and more than 300 commits that can be viewed on Github: https://github.com/GusRot",
+            objectives:
+                "Develop the best user experience through lines of code.",
+            improve:
+                "Javascript/ Typescript / ReactJS /HTML / CSS/ SASS / Bootstrap.",
+        },
+    },
+];
+
+const someProjects = [
+    {
+        title: "Adobe Commerce – Front-end Development",
+        period: "Desenvolvimento Magento",
+        ativities: [
+            "HTML, CSS, JS, XML. (basic PHP).",
+            "Less pre-processor, CSS architecture patterns, Mixin modeling, UI responsive.",
+            "JQuery library, JQuery Widgets, Ajax requisitions, asynchronous functions.",
+            "UiComponents and Knockout(ko) solutions.",
+            "SEO, accessibility, and performance in production environment.",
+            "Development of CMS blocks and page builder components.",
+            "Contributing to daily meetings, sprint planning and estimations.",
+            "Git versioning and code reviews.",
+            `Meus repositórios pessoais: 
+            <a class="links" href="https://github.com/GusRot/Magento-Quick-Learn" target="blank">Repo1 </a>
+            <a class="links" href="https://github.com/GusRot/Magento-Theme" target="blank">Repo2 </a>
+            <a class="links" href="https://github.com/GusRot/Magento-Styles" target="blank">Repo3</a>
+            `,
+        ],
+    },
+    {
+        title: "Project dt-money",
+        period: "React Development",
+        activities: [
+            "Function Components as standard.",
+            "Typescript for greater control over variables in the application.",
+            "Context to control transactions with global variables.",
+            "Requests from UserTransaction API with the Axios library.",
+            "Use of Storage to save data saved by the user.",
+            `Deploy with Vercel: <a class="links" href="https://dt-money-one.vercel.app" target="blank">Build Link</a> Repository: <a class="links" href="https://github.com/GusRot/DTMoney" target="blank">Link Repo</a>`,
+        ],
+    },
+    {
+        title: "Project e-commerce",
+        period: "React Development",
+        activities: [
+            "Class Components as standard.",
+            "REDUX to control products in cart with global variables.",
+            "Requests with the Apollo client from an endpoint developed in GRAPHQL.",
+            "Styled components to handle states by props, like dark-mode.",
+            "React-router-dom to handle routes(PLP, PDP, CartPage).",
+            `Deploy with Vercel (<a class="links" href="https://github.com/GusRot/e-commerce" target="blank">ler instructions on repository</a>):`,
+        ],
+    },
+    {
+        title: "Project portfolio ",
+        period: "React Development - All of my projects in one link. ",
+        activities: [
+            `to visualize build of project. <a class="links" href="https://portifolio-gusrot.vercel.app" target="blank">[Access link here]</a> `,
+        ],
+    },
+];
+
 const courses = [
     {
         name: "Front-End Developer ReactJS",
         institution: "Alura – December / 2021",
         description:
             "Develop a front-end application with the technologies listed in the certificate",
-        link: "https://onedrive.live.com/embed?cid=5AC5D917DF9B44FB&resid=5AC5D917DF9B44FB%21156023&authkey=ACTcfT5ENlpL_I4&em=2",
+        link: "https://1drv.ms/b/s!AvtEm98X2cVanfpDQeb6PJ1471Aj8g?e=2RA5Jj",
+    },
+    {
+        name: "Magento Front-end",
+        institution: "M.Academy – 2022",
+        description:
+            "Develop Adobe-Commerce applications using: UiComponents / JS / KO / AJAX",
+        link: "https://1drv.ms/u/s!AvtEm98X2cVanf4iRjndKa8BSn9b2w?e=TO6jQz",
     },
     {
         name: "Formulation of a Strategic Plan",
@@ -206,14 +281,14 @@ window.onload = function () {
     }
     for (let i = 0; i < personalLinks.length; i++) {
         personalId.innerHTML += `<li class="linha">${personalLinks[i].name} 
-        <p><a href=${personalLinks[i].href} src=${personalLinks[i].src}></a></p>
+        <p><a href=${personalLinks[i].href} target="blank"><img class="logo" src=${personalLinks[i].src}></img></a></p>
     </li>`;
     }
 
-    const objectivesID = document.getElementById("objectives");
-    for (let i = 0; i < objectives.length; i++) {
-        objectivesID.innerHTML += `<li class="linha">${objectives[i]}</li>`;
-    }
+    // const objectivesID = document.getElementById("objectives");
+    // for (let i = 0; i < objectives.length; i++) {
+    //     objectivesID.innerHTML += `<li class="linha">${objectives[i]}</li>`;
+    // }
 
     const languagesID = document.getElementById("languages");
     for (let i = 0; i < languages.length; i++) {
@@ -230,12 +305,12 @@ window.onload = function () {
         <button class="show-more-less" onclick = "toggleMoreLess(this)">Ver menos -</button>
         <div>
             <p class="arrow-before"><em>
-                <strong>Atividades:</strong>${professional[i].ativities.ativitie}<br>
-                <strong>Objetivos:</strong> ${professional[i].ativities.objectives}<br>
-                <strong>Aprendizados:</strong> ${professional[i].ativities.improve}
+                <strong>activities:</strong>${professional[i].activities.activity}<br>
+                <strong>Objective:</strong> ${professional[i].activities.objectives}<br>
+                <strong>Improved:</strong> ${professional[i].activities.improve}
             </em></p>
         </div>
-    </li> `;
+    </li> <br>`;
     }
 
     const coursesID = document.getElementById("courses");
@@ -260,3 +335,39 @@ window.onload = function () {
     </li>`;
     }
 };
+
+const professionalCompID = document.getElementById("professionalComplementary");
+for (let i = 0; i < professionalComplementary.length; i++) {
+    professionalCompID.innerHTML += `<li>
+        <p><strong>${professionalComplementary[i].title}</strong>: ${professionalComplementary[i].period} </p>
+        <button class="show-more-less" onclick = "toggleMoreLess(this)">Ver menos -</button>
+        <div>
+            <p class="arrow-before"><em>
+                <strong>activities:</strong>${professionalComplementary[i].activities.activity}<br>
+                <strong>Objective:</strong> ${professionalComplementary[i].activities.objectives}<br>
+                <strong>Improved:</strong> ${professionalComplementary[i].activities.improve}
+            </em></p>
+        </div>
+    </li>         <br>
+    `;
+}
+
+const projectsReact = document.getElementById("projectsReact");
+for (let i = 0; i < someProjects.length; i++) {
+    projectsReact.innerHTML += `<li>
+        <p><strong>${someProjects[i].title}</strong>: ${someProjects[i].period} </p>
+        <button class="show-more-less" onclick = "toggleMoreLess(this)">Ver menos -</button>
+        <div id="activityProject${i}">
+        </div>
+        <br>
+    </li> `;
+
+    const activityProject = document.getElementById(`activityProject${i}`);
+    for (let j = 0; j < someProjects[i].activities.length; j++) {
+        activityProject.innerHTML += `
+            <p class="arrow-before">
+                <em>${someProjects[i].activities[j]}</em>
+            </p>
+        `;
+    }
+}
